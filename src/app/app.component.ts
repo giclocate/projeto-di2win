6586ca7abdd561b3d3484a36266286c4a6031970
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-05-25 16:35:56 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
   title = 'projeto-di2win';
