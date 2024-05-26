6bcbdfefeba1f6ace4f0f1c6f91566513c187a7d
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-05-26 20:24:53 -0300

    feat: adiciona a barra lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import {MatIcon} from '@angular/material/icon';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    imports: [RouterOutlet, HeaderComponent,MatIcon ]
+    imports: [RouterOutlet, HeaderComponent,MatIcon, SidebarComponent ]
 })
 export class AppComponent {
   title = 'projeto-di2win';
